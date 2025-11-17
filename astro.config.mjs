eaabb093fb03640fcdb1e76a7a5152df3067b145
William Kim <wik002@ucsd.edu>
2025-11-16 18:37:52 -0800

    Added visual gallery with scrollTo features. Cleaned up other code

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,8 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import pagefind from "astro-pagefind"
+import collection_search from 'astro-collection-search';
+
 import tailwindcss from "@tailwindcss/vite";
 import mdx from "@astrojs/mdx";
 import rehypePrettyCode from 'rehype-pretty-code';
@@ -10,7 +12,7 @@ import rehypeSlug from 'rehype-slug';
 // https://astro.build/config
 export default defineConfig({
   // add to your existing integrations
-  integrations: [pagefind(), mdx({
+  integrations: [pagefind(),collection_search(), mdx({
     syntaxHighlight: false,
     rehypePlugins: [
       rehypeSlug,
