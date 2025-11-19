4cc8b4fee7405e23b774377eba1f22d60d4db49e
William Kim <wik002@ucsd.edu>
2025-11-19 00:14:54 -0800

    improved styling and added static site searching capabilities with pagefind

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,6 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-import pagefind from "astro-pagefind"
-import collection_search from 'astro-collection-search';
+import { plainTextPlugin } from "@barnabask/astro-minisearch";
 
 import tailwindcss from "@tailwindcss/vite";
 import mdx from "@astrojs/mdx";
@@ -12,7 +11,11 @@ import rehypeSlug from 'rehype-slug';
 // https://astro.build/config
 export default defineConfig({
   // add to your existing integrations
-  integrations: [pagefind(),collection_search(), mdx({
+  markdown: {
+    // @ts-ignore
+    rehypePlugins: [plainTextPlugin()],
+  },
+  integrations: [ mdx({
     syntaxHighlight: false,
     rehypePlugins: [
       rehypeSlug,
